b50ef21c79ce2d11578384e1a524180265f3e669
Chrisdocs <chrisa.wolfe@gmail.com>
2021-07-26 10:11:32 -0500

    commit

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -56,27 +56,20 @@ const commentController = {
     },
 
     // remove comment
-    removeComment({
-        params
-    }, res) {
-        Comment.findOneAndDelete({
-                _id: params.commentId
-            })
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete(
+            { _id: params.commentId} )
             .then((deletedComment) => {
                 if (!deletedComment) {
                     return res.status(404).json({
                         message: "No comment with this id!"
                     });
                 }
-                return Pizza.findOneAndUpdate({
-                    _id: params.pizzaId
-                }, {
-                    $pull: {
-                        comments: params.commentId
-                    }
-                }, {
-                    new: true
-                });
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $pull: { comments: params.commentId } },
+                    { new: true}
+                    );
             })
             .then((dbPizzaData) => {
                 if (!dbPizzaData) {
